bd6c7ead5dd9a67424091ce2d2e1e01a7238c301
HyeonSang Cho <wolf1323@naver.com>
2023-10-11 20:17:30 +0900

    real_end

--- boy.py
+++ boy.py
@@ -148,7 +148,7 @@ class StateMachine:
             Idle: { right_down: Run,left_down: Run,left_up: Run,right_up: Run,time_out : Sleep,a_down: AutoRun},
             Run: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle,a_down: AutoRun},
             Sleep: {right_down: Run, left_down: Run, right_up: Run, left_up: Run, space_down: Idle,a_down: AutoRun},
-            AutoRun: {a_down: AutoRun,time_out : Idle,}
+            AutoRun: {a_down: AutoRun,time_out : Idle}
         }
 
     def handle_event(self, e):
